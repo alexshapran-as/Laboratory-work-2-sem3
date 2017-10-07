a71858b48243b492adb91225133e0d8002d54384
Alex Shapran <shapran400@gmail.com>
2017-10-08 00:48:19 +0300

    Delete NonCopMovable.cpp

--- NonCopMovable.cpp
+++
@@ -1,9 +0,0 @@
-#include "NonCopMovable.h"
-#include <iostream>
-#include <string>
-
-std::ostream& operator<<(std::ostream& os, const NonCopMovable& obj)
-{
-	os << std::endl << obj.str << std::endl;
-	return os;
-}
