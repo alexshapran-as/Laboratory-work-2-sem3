9163d8e96944c26cba99b8296a58c6d13ce70ab4
Alex Shapran <shapran400@gmail.com>
2017-10-08 00:44:43 +0300

    Update NonCopMovable.cpp

--- NonCopMovable.cpp
+++ NonCopMovable.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <string>
 
-NonCopMovable::NonCopMovable(const std::string _str) : str(_str) {}
 std::ostream& operator<<(std::ostream& os, const NonCopMovable& obj)
 {
 	os << std::endl << obj.str << std::endl;
